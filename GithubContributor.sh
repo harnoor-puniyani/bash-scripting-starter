df7d7d44a4bc08eda203512d400dc24a5e9d30f3
Harnoor <18042000harjee@gmail.com>
2025-05-04 07:50:03 +0000

    minor fixes and addedd helper function

--- GithubContributor.sh
+++ GithubContributor.sh
@@ -18,6 +18,8 @@
 #set -exo pipefail
 ################################
 
+#helper()
+
 API_BASE_URL=https://api.github.com
 
 #Format - Baseurl/repos/ORG/REPO/collaborators
@@ -29,8 +31,8 @@ USER=$GIT_USERNAME
 # Fetching Arguments
 OWNER=$1
 REPO=$2
-include_admins="${3:=false}"
-
+include_admins=${3:-'false'}
+#echo $include_admins
 url="$API_BASE_URL/repos/$OWNER/$REPO/collaborators"
 
 function base_caller(){
@@ -40,13 +42,13 @@ response=`curl -sS --fail -L -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $TOKEN" \
   -H "X-GitHub-Api-Version: 2022-11-28" $url`
 
-if(( $?  == 0));then
+if (( $?  == 0));then
 	echo "collaborators are listed below for $OWNER/$REPO"
-	if(( $include_admins == false ));then
+	if [ $include_admins = "false" ];then
 		echo $response | jq -r '.[] | select(.permissions.admin == false) .login'
 	else
 		echo $response | jq '.[].login'
-	fi
+	fi;
 else
 	echo "error occured"
 fi;
@@ -56,8 +58,14 @@ fi;
 
 function helper(){
 #write helper for args
-echo "hellow"
+#echo "hellow"
+if [ $1 -lt 2 ];
+then
+	echo "Positional params provided : $1"
+	echo "provide parameters for Owner and repository"
+	echo "[optional] proide parameter to include admins"
+fi;
 }
-
+helper $#
 base_caller
 
